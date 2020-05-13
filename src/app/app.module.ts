82cd865ed05a9b23faa2dda0716cb17a4f8c62e4
Afanasiy Illarionov <illarionoff.a@gmail.com>
2020-05-13 23:01:00 +0300

    Add main layout routing and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { MainLayoutComponent } from "./shared/components/main-layout/main-layout.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { PostPageComponent } from "./post-page/post-page.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    PostPageComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
